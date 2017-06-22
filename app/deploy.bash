d14a95972ea942a37e2c3665baba8ed8a2dbb044
freejoe76 <jmurphy@denverpost.com>
2017-06-22 17:49:09 -0400

    Add deploy scripts and functionality

---
+++ app/deploy.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+# scp flat files to prod.
+ENVIRON='PROD'
+export environ=$ENVIRON
+SERVER='qa'
+DIR='/path/to/project/root/'
+PROJECT='projectname'
+LOCATION_OVERRIDE=0
+html_only() { 
+    echo ''
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -e | --environ ) shift
+            ENVIRON=$1
+            ;;
+        -p | --project ) shift
+            PROJECT=$1
+            ;;
+        -s | --server ) shift
+            SERVER=$1
+            ;;
+        -d | --dir ) shift
+            DIR=$1
+            ;;
+        -h | --htmlonly ) shift
+            html_only() {
+                find public \! -name "*.html" -type f -delete;
+            }
+            ;;
+        -f | --freeze ) shift
+            python freeze.py; export environ='DEV'; exit 1
+            ;;
+    esac
+    shift
+done
+
+# If we bailed on deploy mid-deploy we'll still have the public dir around.
+if [ -d "$PROJECT" ]; then rm -fr $PROJECT; fi
+
+#python tests.py && \
+html_only && \
+    python freeze.py && \
+    mv build $PROJECT && \
+    scp -r $PROJECT $SERVER:$DIR && \
+    mv $PROJECT build
+
+export environ='DEV'
